ea319382111e106fde2347fc68f33e6c88814190
Kartik Neti <kartikprajak@gmail.com>
2023-10-21 13:50:12 +0530

    Added functionality to browse multiple files

--- projects_python/compress_app.py
+++ projects_python/compress_app.py
@@ -1,17 +1,23 @@
 import PySimpleGUI as pg
+import zip_creator as zc
 
 label1 = pg.Text("Choose file to compress.")
 textbox1 = pg.Input()
-choosebutton1 = pg.FilesBrowse()
+choosebutton1 = pg.FilesBrowse("Select", key='filepath')
 
 label2 = pg.Text("Choose destination folder.")
 textbox2 = pg.Input()
-choosebutton2 = pg.FolderBrowse()
+choosebutton2 = pg.FolderBrowse("Select", key='folderpath')
 
 compress = pg.Button("Compress")
 
 window = pg.Window("File Compressor", layout=[[label1,textbox1,choosebutton1],[label2,textbox2,choosebutton2],[compress]])
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepath = values['filepath']
+    folderpath = values['folderpath']
+    zc.make_archive(filepath, folderpath)
 window.close()
 
 
---
+++ projects_python/zip_creator.py
@@ -0,0 +1,9 @@
+import zipfile as z
+
+def make_archive(filepath, folderpath):
+    with z.ZipFile(folderpath, 'x') as file:
+        for filepath in filepath:
+            file.write(filepath)
+            
+if __name__ == '__main__':
+    def make_archive()
